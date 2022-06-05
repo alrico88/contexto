18fa24db0b3410106bcbf5e457b2f673242c3ea1
Alberto Rico <alrico88@gmail.com>
2022-06-05 15:46:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+![contexto icon](./.github/images/logo.svg#gh-dark-mode-only)
+![contexto icon](./.github/images/logo-dark.svg#gh-light-mode-only)
+
+# contexto
+
+A MacOS menu bar app to quickly switch between Docker contexts. Built using Electron.
+
+![contexto screnshot](https://i.imgur.com/ZcPTpVo.png)
+
+## Usage
+
+Clone this repo and run `pnpm install`.
+
+To build the app run `pnpm run build`.
+
+Move the built app to /Applications.
+
+On app execution, a menu bar item will show the existing Docker contexts, and clicking one will switch to the selected one.
+
+Docker must be running prior to running Contexto.
+
+## Features
+
+[x] List existing contexts
+[x] Switch to selected context
+[ ] Manage contexts (create / delete)
+[ ] Open on system startup
+[ ] Customize Docker executable path
+[ ] Linux and Windows support
---
+++ main.js
@@ -0,0 +1,90 @@
+const { app, Menu, Tray, nativeImage } = require('electron');
+const { exec } = require('child_process');
+const path = require('path');
+
+function executeCommand(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        reject(error.message);
+        return;
+      }
+      if (stderr) {
+        reject(stderr);
+        return;
+      }
+
+      resolve(stdout);
+    });
+  });
+}
+
+let tray = null;
+
+const quitApp = () => app.quit();
+
+// TODO: Get executable from path / multi OS
+const dockerExecutable =
+  '/Applications/Docker.app/Contents/Resources/bin/docker';
+
+function changeContext(contextName) {
+  return executeCommand(`${dockerExecutable} context use ${contextName}`);
+}
+
+async function getDockerContexts() {
+  const list = await executeCommand(
+    `${dockerExecutable} context ls --format=json`,
+  );
+  const parsed = JSON.parse(list);
+
+  return parsed.map((d) => {
+    return {
+      label: d.Name,
+      checked: d.Current,
+      type: 'radio',
+      click: async ({ label }) => {
+        await changeContext(label);
+        tray.setTitle(label);
+      },
+    };
+  });
+}
+
+const permanentItems = [
+  { label: '', type: 'separator' },
+  { label: 'Refresh contexts', type: 'normal', click: updateTray },
+  { label: 'Close', type: 'normal', click: quitApp },
+];
+
+async function updateTray() {
+  let contexts;
+
+  try {
+    contexts = await getDockerContexts();
+  } catch (err) {
+    console.error(err);
+    contexts = [];
+  }
+
+  const contextMenu = Menu.buildFromTemplate([...contexts, ...permanentItems]);
+
+  tray.setContextMenu(contextMenu);
+  tray.setTitle(contexts.find((d) => d.checked).label);
+}
+
+const image = nativeImage.createFromPath(path.join(__dirname, 'icon.png'));
+
+app
+  .whenReady()
+  .then(async () => {
+    if (app.dock) {
+      app.dock.hide();
+    }
+
+    tray = new Tray(image.resize({ width: 16, height: 16 }));
+    tray.setToolTip('Contexto');
+    updateTray();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
